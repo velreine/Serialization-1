e35a7dce4371735b00109e42f3206968ecee6ce0
Velreine <Velreine@VELREINE-ASUS>
2017-11-01 08:37:43 +0100

    Master commit

--- Serialization 1/Program.cs
+++ Serialization 1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,46 @@ namespace Serialization_1
     {
         static void Main(string[] args)
         {
+
+            
+
+            BinaryFormatter bf = new BinaryFormatter();
+
+            FileStream fs = new FileStream("C:\\Users\\Velreine\\Desktop\\seri2.txt", FileMode.Create);
+
+
+            byte[] data = { (byte)10, (byte)11, (byte)12 };
+            Packet myclass = new Packet("Erik", 24, data);
+
+            bf.Serialize(fs, myclass);
+
+            fs.Close();
+
+            int CC;
+
+            FileStream fs2 = new FileStream("C:\\Users\\Velreine\\Desktop\\seri2.txt", FileMode.Open);
+
+            Packet desi;
+
+            desi = (Packet)bf.Deserialize(fs2);
+
+            Console.WriteLine(desi.ToString());
+
+            byte[] newdata = desi.Payload;
+
+            Console.ReadKey();
+
+
+        
+            
         }
+
+
+
+
+
+
+
+
     }
 }
---
+++ Serialization 1/SClass.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Serialization_1
+{
+    [Serializable()]
+    public class Packet : ISerializable
+    {
+
+        public string Name;
+        public int Age;
+        public int Length { get { return this.Payload.Length; } }
+        public byte[] Payload;
+
+
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name", Name, typeof(string));
+            info.AddValue("Age", Age, typeof(int));
+            info.AddValue("Length", Length, typeof(int));
+
+            int num = 0;
+            foreach (byte b in Payload)
+            {
+                string key = "_data" + Convert.ToString(num);
+                info.AddValue(key, b, typeof(byte));
+                num++;
+
+            }
+
+
+        }
+
+        public Packet(SerializationInfo info, StreamingContext context)
+        {
+            this.Name = (string)info.GetValue("Name", typeof(string));
+            this.Age = (int)info.GetValue("Age", typeof(int));
+
+            int payloadLength = (int)info.GetValue("Length", typeof(int));
+            byte[] data = new byte[payloadLength];
+
+            for (int i = 0; i < payloadLength; i++)
+            {
+                data[i] = (byte)info.GetValue("_data" + i, typeof(byte));
+            }
+
+            this.Payload = data;
+
+
+        }
+
+        public Packet(string name, int age, byte[] payload)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Payload = payload;
+
+        }
+
+
+        public void Serialize(System.Net.Sockets.NetworkStream stream, Packet obj)
+        {
+            System.Runtime.Serialization.Formatters.Binary.BinaryFormatter bf = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+            bf.Serialize(stream, obj);
+            
+
+        }
+
+        public Packet Deserialize(System.Net.Sockets.NetworkStream stream)
+        {
+            System.Runtime.Serialization.Formatters.Binary.BinaryFormatter bf = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+            return (Packet)(bf.Deserialize(stream));
+
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + " Age: " + Convert.ToString(Age);
+        }
+
+       
+
+
+    }
+}
