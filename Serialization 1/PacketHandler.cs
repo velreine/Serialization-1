1952d99ba5dda8d7ec4c622a3c5b380fbf521bfb
velreine <nicky-g-hansen@hotmail.com>
2017-11-02 23:40:03 +0100

    Added some comments

--- Serialization 1/PacketHandler.cs
+++ Serialization 1/PacketHandler.cs
@@ -5,6 +5,90 @@ using System.Text;
 using System.Threading.Tasks;
 
 
+/* WorldPacket : ByteBuffer
+ * 
+ * Methods::
+ * GetOpcode() const
+ * GetOpcodeName() const 
+ * Initialize(uint 16 opcode, size_t newres = 200)
+ * SetOpcode(uint16 opcode)
+ * 
+ * 
+ * Constructors::
+ * WorldPacket()
+ * WorldPacket(const WorldPacket & packet)
+ * WorldPacket(uint16 opcode, size_t res = 200)
+ * 
+ * Vars::
+ * m_opcode
+ * 
+ * */
+/* ByteBuffer
+ * 
+ * 
+ * Methods::
+ * 
+ * append(const ByteBuffer &buffer)
+ * append(const char * src, size_t cnt)
+ * append(const std::string &str)
+ * append(const uint8 * src, size_t cnt)
+ * append<T>(const T * src, size_t cnt)
+ * append<T>(T value)
+ * appendPackGUID(uint64 guid)
+ * appendPackXYZ(float x, float y, float z)
+ * clear()
+ * contents() const
+ * empty() const
+ * hexlike() const
+ * print_storage() const
+ * put(size_t pos, const uint8 * src, size_t cnt)
+ * put<T>(size_t pos, T value)
+ * read(uint8 * dest, size_t len)
+ * read<T>()
+ * read<T>(size_t pos) const
+ * read_skip(size_t skip)
+ * read_skip<T>()
+ * readPackGUID()
+ * reserve(size_t ressize)
+ * resize(size_t newsize)
+ * rpos() const
+ * size() const
+ * textlike() const
+ * wpos() const
+ * wpos(size_t wpos_)
+ * 
+ * 
+ * 
+ * 
+ * Constructors:::
+ * 
+ * ByteBuffer()
+ * ByteBuffer(const ByteBuffer & buf)
+ * ByteBuffer(size_t res)
+ *
+ * Variables::
+ * 
+ * DEFAULT_SIZE
+ * _rpos
+ * _storage
+ * _wpos
+ *
+ * Overloaded operators::: 
+ * 
+ * []
+ * <<
+ * >>
+ *
+ */
+
+
+
+
+
+
+
+
+
 namespace Serialization_1
 {
     public class PacketHandler
